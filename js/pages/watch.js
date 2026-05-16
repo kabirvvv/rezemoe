eb941e85741ac7ac62c1368bc484ce68a633cf63
kabirvvv <kabirv338@gmail.com>
2026-05-16 21:51:26 +0530

    Update watch.js

--- js/pages/watch.js
+++ js/pages/watch.js
@@ -180,7 +180,7 @@ const updateEpLabel = () => {
 
     try {
       const apiUrl = `https://reanime.to/api/flix/${encodeURIComponent(currentAnimeId)}/${currentEpNum}`;
-      const res = await fetch(apiUrl);
+      const res = await fetch(API.proxy(apiUrl));
       if (!res.ok) throw new Error(`API error ${res.status}`);
       const data = await res.json();
 
@@ -219,4 +219,3 @@ const updateEpLabel = () => {
 })();
 
 window.WatchPage = WatchPage;
-    
